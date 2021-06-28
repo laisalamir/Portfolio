13860c9223e3d85b22dfbf75af96eea57a2e4e51
laisalamir <lmslamir@gmail.com>
2021-06-28 16:02:43 +0100

    atualizações componente blog

---
+++ src/app/blog/blog.component.js
@@ -0,0 +1,5 @@
+document.getElementById("upbutton")
+        .addEventListener("click", function() {
+  document.getElementById("upbutton").hidden = true;
+  document.getElementById("update").hidden = false;
+}, false);
